4b055fcd097e05c2578021921ee28e129a67b8dc
brunoluiz <bruno_d_luiz@estudante.sesisenai.org.br>
2026-08-24 10:58:40 -0300

    add - database

--- database/petshop.sql
+++ database/petshop.sql
@@ -1,19 +1,21 @@
-create database crud_pratos;
+create database crud_clientes;
 use crud_clientes;
 
-create table usuarios (
-    id int primary key auto_increment,
-    nome varchar(100) not null,
-    email varchar(100) not null
+CREATE TABLE clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL,
+
 );
 
-create table pratos (
-    id int primary key auto_increment,
-    nome varchar(100) not null,
-    descricao text not null,
-    preco decimal(10,2) not null,
-    categoria varchar(50) not null,
-
-    id_usuario int not null,
-    foreign key (id_usuario) references usuarios(id)
+CREATE TABLE animais (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    especie VARCHAR(50) NOT NULL,
+    raca VARCHAR(50) NOT NULL,
+    idade INT NOT NULL,
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id) ON DELETE CASCADE
 );
