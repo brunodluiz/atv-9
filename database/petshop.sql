64e97c63f51b99d395c4903be075c85e9c943373
brunoluiz <bruno_d_luiz@estudante.sesisenai.org.br>
2026-08-24 10:50:51 -0300

    add - database

---
+++ database/petshop.sql
@@ -0,0 +1,19 @@
+create database crud_pratos;
+use crud_clientes;
+
+create table usuarios (
+    id int primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null
+);
+
+create table pratos (
+    id int primary key auto_increment,
+    nome varchar(100) not null,
+    descricao text not null,
+    preco decimal(10,2) not null,
+    categoria varchar(50) not null,
+
+    id_usuario int not null,
+    foreign key (id_usuario) references usuarios(id)
+);
---
+++ readme.md
@@ -0,0 +1,9 @@
+O projeto deverá conter:
+
+Código-fonte completo;
+Script SQL do banco de dados;
+CRUD de clientes;
+CRUD de animais;
+Relacionamento entre as tabelas;
+README com instruções para execução;
+Sistema funcionando no ambiente local.
